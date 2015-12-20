ada81460207cb890e984be93520a3e679c9262a3
ಠ_ಠ <_>
2015-12-19 21:40:09 -0600

    Fixing presenter error

--- templates/menu.xml.js
+++ templates/menu.xml.js
@@ -2,7 +2,7 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
   <document>
       <menuBarTemplate>
          <menuBar>
-            <menuItem template="${this.BASEURL}templates/news.xml.js" presentation="menuBarItemPresenter">
+            <menuItem template="${this.BASEURL}templates/menu.xml.js" presentation="menuBarItemPresenter">
                 <title>News</title>
             </menuItem>
             <menuItem template="${this.BASEURL}templates/news.xml.js"  presentation="menuBarItemPresenter">
