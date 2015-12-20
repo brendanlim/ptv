b06dd2ac2d192a985d185a27a614b1ddd7f815d7
ಠ_ಠ <_>
2015-12-20 12:38:47 -0600

    Fixing presenter error

--- templates/news.xml.js
+++ templates/news.xml.js
@@ -9,10 +9,6 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
              <relatedContent> 
                <grid>
                  <section>                  
-                  // <lockup videoURL="http://stream3.news5.ph/aksyon/aksyonplayout/playlist.m3u8">
-                  //    <img src="${this.BASEURL}images/aksyon-news-5.jpg" width="362" height="267" />
-                  //    <title>Aksyon News 5</title>
-                  // </lockup>
                   <lockup videoURL="http://livestream01.untvweb.com:1935/public/untvwebstream/playlist.m3u8">
                      <img src="${this.BASEURL}images/untv.jpg" width="600" height="360" />
                      <title>UNTV</title>
