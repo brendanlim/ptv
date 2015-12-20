0155825f595d2ff965b4c949e30a9b152636604c
ಠ_ಠ <_>
2015-12-19 21:34:15 -0600

    Fixing presenter error

--- js/Presenter.js
+++ js/Presenter.js
@@ -1,4 +1,31 @@
 var Presenter = {
+  defaultPresenter: function(xml) {
+    if(this.loadingIndicatorVisible) {
+        navigationDocument.replaceDocument(xml, this.loadingIndicator);
+        this.loadingIndicatorVisible = false;
+    } else {
+        navigationDocument.pushDocument(xml);
+    }
+  },
+
+  showLoadingIndicator: function(presentation) {
+    if (!this.loadingIndicator) {
+        this.loadingIndicator = this.makeDocument(this.loadingTemplate);
+    }
+      
+    if (!this.loadingIndicatorVisible && presentation != "modalDialogPresenter" && presentation != "menuBarItemPresenter") {
+      navigationDocument.pushDocument(this.loadingIndicator);
+      this.loadingIndicatorVisible = true;
+    }
+  },
+
+  removeLoadingIndicator: function() {
+    if (this.loadingIndicatorVisible) {
+      navigationDocument.removeDocument(this.loadingIndicator);
+      this.loadingIndicatorVisible = false;
+    }
+  },
+
 	makeDocument: function(resource) {
 		if (!Presenter.parser) {
 			Presenter.parser = new DOMParser();
@@ -28,7 +55,9 @@ var Presenter = {
 	load: function(event) {
   	var self = this,
       	ele = event.target,
-      	videoURL = ele.getAttribute("videoURL")
+        templateURL = ele.getAttribute("template"),
+        presentation = ele.getAttribute("presentation"),
+      	videoURL = ele.getAttribute("videoURL");
 
   	if(videoURL) {
 	    var player = new Player();
@@ -39,5 +68,25 @@ var Presenter = {
 	    player.playlist.push(mediaItem);
 	    player.present();
   	}
+
+  	if (templateURL) {
+  		self.showLoadingIndicator(presentation);
+
+      resourceLoader.loadResource(templateURL,
+        function(resource) {
+          if (resource) {
+            var doc = self.makeDocument(resource);
+            doc.addEventListener("select", self.load.bind(self));
+            doc.addEventListener("highlight", self.load.bind(self));
+
+            if (self[presentation] instanceof Function) {
+                self[presentation].call(self, doc, ele);
+            } else {
+                self.defaultPresenter.call(self, doc);
+            }
+          }
+        }
+      );
+    }
 	},
 }
