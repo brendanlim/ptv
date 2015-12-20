0a6c920216e8b4807cf9debbc4a3b26196774449
ಠ_ಠ <_>
2015-12-19 21:26:28 -0600

    wtf

--- templates/menu.xml.js
+++ templates/menu.xml.js
@@ -1,20 +1,20 @@
 var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
-<document>
-   <menuBarTemplate>
-      <menuBar>
-         <menuItem template="${this.BASEURL}templates/news.xml.js" 
-           presentation="menuBarItemPresenter">
-             <title>News</title>
-         </menuItem>
-         <menuItem template="${this.BASEURL}templates/radio.xml.js" 
-           presentation="menuBarItemPresenter">
-            <title>Radio</title>
-         </menuItem>
-         <menuItem template="${this.BASEURL}templates/about.xml.js" 
-           presentation="menuBarItemPresenter">
-            <title>About</title>
-         </menuItem>
-      </menuBar>
-   </menuBarTemplate>
-</document>`
+  <document>
+      <menuBarTemplate>
+         <menuBar>
+            <menuItem template="${this.BASEURL}templates/news.xml.js" 
+              presentation="menuBarItemPresenter">
+                <title>News</title>
+            </menuItem>
+            <menuItem template="${this.BASEURL}templates/news.xml.js" 
+              presentation="menuBarItemPresenter">
+               <title>Music</title>
+            </menuItem>
+            <menuItem template="${this.BASEURL}templates/news.xml.js" 
+              presentation="menuBarItemPresenter">
+               <title>About</title>
+            </menuItem>
+         </menuBar>
+      </menuBarTemplate>
+   </document>`
 }
