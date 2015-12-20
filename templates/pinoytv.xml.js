d1680a665f172fe9a014da5888e51744e43f5ae5
ಠ_ಠ <_>
2015-12-19 21:12:00 -0600

    Aksyon news 5 added

--- templates/pinoytv.xml.js
+++ templates/pinoytv.xml.js
@@ -7,12 +7,15 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
       <list> 
         <section> 
            <listItemLockup> 
-             <title>Networks</title>
-             <decorationLabel>13</decorationLabel>
-                  //1. add from here
+             <title>News</title>
+             <decorationLabel></decorationLabel>
              <relatedContent> 
                <grid>
-                 <section> 
+                 <section>                  
+                  <lockup videoURL="http://stream3.news5.ph/aksyon/aksyonplayout/playlist.m3u8">
+                     // <img src="${this.BASEURL}images/untv.jpg" width="600" height="360" />
+                     <title>Aksyon News 5</title>
+                  </lockup>
                   <lockup videoURL="http://livestream01.untvweb.com:1935/public/untvwebstream/playlist.m3u8">
                      <img src="${this.BASEURL}images/untv.jpg" width="600" height="360" />
                      <title>UNTV</title>
