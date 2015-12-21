cdb38fd35de5b1c8d6fd7ceb1e04b60a303bbdc3
ಠ_ಠ <_>
2015-12-21 16:05:50 -0600

    Updating background color

--- templates/pinoytv.xml.js
+++ templates/pinoytv.xml.js
@@ -19,7 +19,7 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
   <stackTemplate class="darkBackgroundColor" theme="dark">
     <banner>
       <background>
-        <img src="${this.BASEURL}images/header.jpg" width="1920" height="360" />
+        <img src="${this.BASEURL}images/header.jpg" width="1920" height="175" />
       </background>
       <title class="titleColor">Pinoy Television</title>
       <description>An always updating collection of Filipino TV and music across the internet. Check often for new TV and music selections.</description>
