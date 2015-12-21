439fe6d53eff7b179420b30fa845ccaab0195212
ಠ_ಠ <_>
2015-12-21 15:44:25 -0600

    Adding header

--- templates/pinoytv.xml.js
+++ templates/pinoytv.xml.js
@@ -15,7 +15,7 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
   <stackTemplate class="darkBackgroundColor" theme="dark">
     <banner>
       <background>
-        <img src="${this.BASEURL}resources/images/iceland/iceland_wide.jpg" width="1920" height="360" />
+        <img src="${this.BASEURL}resources/images/header.jpg" width="1920" height="360" />
       </background>
       <description>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</description>
     </banner>
