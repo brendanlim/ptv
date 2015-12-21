116f5da2b4d6e75402a1efcc1c2e1c36d3945c5a
ಠ_ಠ <_>
2015-12-21 15:13:31 -0600

    Dark theme

--- templates/news.xml.js
+++ templates/news.xml.js
@@ -1,6 +1,6 @@
 var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
   <document>
-    <catalogTemplate> 
+    <catalogTemplate theme="dark"> 
       <banner> 
         <title>Live TV</title>
       </banner>
