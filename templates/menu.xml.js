ffb513207a791b35e91f67b50acecee5c76de466
ಠ_ಠ <_>
2015-12-21 14:54:25 -0600

    Attempting to change news layout

--- templates/menu.xml.js
+++ templates/menu.xml.js
@@ -2,13 +2,13 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
 <document>
   <menuBarTemplate>
     <menuBar>
-      <menuItem template="${this.BASEURL}templates/pinoytv.xml.js" presentation="menuBarItemPresenter">
+      <menuItem template="${this.BASEURL}templates/news.xml.js" presentation="menuBarItemPresenter">
         <title>News</title>
       </menuItem>
-      <menuItem template="${this.BASEURL}templates/pinoytv.xml.js" presentation="menuBarItemPresenter">
+      <menuItem template="${this.BASEURL}templates/news.xml.js" presentation="menuBarItemPresenter">
         <title>Music</title>
       </menuItem>
-      <menuItem template="${this.BASEURL}templates/pinoytv.xml.js" presentation="menuBarItemPresenter">
+      <menuItem template="${this.BASEURL}templates/news.xml.js" presentation="menuBarItemPresenter">
         <title>About</title>
       </menuItem>
     </menuBar>
