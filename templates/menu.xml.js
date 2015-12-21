5a8ed77292b283dca15c46df2d603a23710ae680
ಠ_ಠ <_>
2015-12-21 15:29:46 -0600

    Fixing selection issue again

--- templates/menu.xml.js
+++ templates/menu.xml.js
@@ -1,6 +1,6 @@
 var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
 <document>
-  <menuBarTemplate>
+  <menuBarTemplate theme="extraLight">
     <menuBar>
       <menuItem template="${this.BASEURL}templates/news.xml.js" presentation="menuBarItemPresenter">
         <title>Live TV</title>
--- templates/news.xml.js
+++ templates/news.xml.js
@@ -1,6 +1,6 @@
 var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
   <document>
-    <catalogTemplate> 
+    <catalogTemplate theme="extraLight"> 
       <banner> 
         <title>Live TV</title>
       </banner>
