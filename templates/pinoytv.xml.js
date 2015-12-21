ee4e1a1cc75336ba414ca50494f78479050061ea
ಠ_ಠ <_>
2015-12-21 15:40:26 -0600

    Swappp!'

--- templates/pinoytv.xml.js
+++ templates/pinoytv.xml.js
@@ -1,31 +1,58 @@
 var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
-  <document>
-    <catalogTemplate> 
-      <banner> 
-        <title>Pinoy TV</title>
-      </banner>
-      <list> 
-        <section> 
-           <listItemLockup> 
-             <title>News</title>
-             <decorationLabel></decorationLabel>
-             <relatedContent> 
-               <grid>
-                 <section>                  
-                  <lockup videoURL="http://stream3.news5.ph/aksyon/aksyonplayout/playlist.m3u8">
-                     <img src="${this.BASEURL}images/aksyon-news-5.jpg" width="362" height="267" />
-                     <title>Aksyon News 5</title>
-                  </lockup>
-                  <lockup videoURL="http://livestream01.untvweb.com:1935/public/untvwebstream/playlist.m3u8">
-                     <img src="${this.BASEURL}images/untv.jpg" width="600" height="360" />
-                     <title>UNTV</title>
-                  </lockup>
-                 </section>
-               </grid>
-             </relatedContent>
-           </listItemLockup>
+<document>
+  <head>
+    <style>
+    .showTextOnHighlight {
+      tv-labels-state: show-on-highlight;
+    }
+    @media -tv-template and (-tv-uber) {
+      .darkBackgroundColor {
+        background-color: rgb(4, 27, 97);
+      }
+    }
+    </style>
+  </head>
+  <stackTemplate class="darkBackgroundColor" theme="dark">
+    <banner>
+      <background>
+        <img src="${this.BASEURL}resources/images/iceland/iceland_wide.jpg" width="1920" height="360" />
+      </background>
+      <description>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</description>
+    </banner>
+    <collectionList>
+      <shelf>
+        <header>
+          <title>Television</title>
+        </header>
+        <section>
+          <lockup videoURL="http://livestream01.untvweb.com:1935/public/untvwebstream/playlist.m3u8">
+             <img src="${this.BASEURL}images/tv/untv.jpg" width="308" height="308" />
+             <title class="showTextOnHighlight">UNTV</title>
+          </lockup>
+
+          <lockup videoURL="http://stream3.news5.ph/aksyon/aksyonplayout/playlist.m3u8">
+             <img src="${this.BASEURL}images/tv/aksyon-news-5.jpg" width="308" height="308" />
+             <title class="showTextOnHighlight">Aksyon News 5</title>
+          </lockup>
         </section>
-      </list>
-    </catalogTemplate>
-  </document>`
+      </shelf>
+      <shelf>
+        <header>
+          <title>Radio</title>
+        </header>
+        <section>
+          <lockup videoURL="http://livestream01.untvweb.com:1935/public/untvwebstream/playlist.m3u8">
+             <img src="${this.BASEURL}images/tv/untv.jpg" width="308" height="308" />
+             <title class="showTextOnHighlight">UNTV</title>
+          </lockup>
+
+          <lockup videoURL="http://stream3.news5.ph/aksyon/aksyonplayout/playlist.m3u8">
+             <img src="${this.BASEURL}images/tv/aksyon-news-5.jpg" width="308" height="308" />
+             <title class="showTextOnHighlight">Aksyon News 5</title>
+          </lockup>
+        </section>
+      </shelf>
+    </collectionList>
+  </stackTemplate>
+</document>`
 }
