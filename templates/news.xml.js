3704ae157744e6b7513be6206cc0f835b2252864
ಠ_ಠ <_>
2015-12-21 15:08:25 -0600

    Attempting to change news layout

--- templates/news.xml.js
+++ templates/news.xml.js
@@ -1,19 +1,22 @@
 var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
   <document>
-     <grid margin="20">
-        <header>
-          <title>News</title>
-        </header>
-       <section>                  
-        <lockup videoURL="http://stream3.news5.ph/aksyon/aksyonplayout/playlist.m3u8">
-           <img src="${this.BASEURL}images/aksyon-news-5.jpg" width="362" height="267" />
-           <title>Aksyon News 5</title>
-        </lockup>
-        <lockup videoURL="http://livestream01.untvweb.com:1935/public/untvwebstream/playlist.m3u8">
-           <img src="${this.BASEURL}images/untv.jpg" width="600" height="360" />
-           <title>UNTV</title>
-        </lockup>
-       </section>
-     </grid>
+    <catalogTemplate> 
+      <list> 
+        <section> 
+               <grid>
+                 <section>                  
+                  <lockup videoURL="http://stream3.news5.ph/aksyon/aksyonplayout/playlist.m3u8">
+                     <img src="${this.BASEURL}images/aksyon-news-5.jpg" width="362" height="267" />
+                     <title>Aksyon News 5</title>
+                  </lockup>
+                  <lockup videoURL="http://livestream01.untvweb.com:1935/public/untvwebstream/playlist.m3u8">
+                     <img src="${this.BASEURL}images/untv.jpg" width="600" height="360" />
+                     <title>UNTV</title>
+                  </lockup>
+                 </section>
+               </grid>
+        </section>
+      </list>
+    </catalogTemplate>
   </document>`
 }
