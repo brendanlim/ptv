abece0d276f7724faecf88ee081550fbe0b0b273
ಠ_ಠ <_>
2015-12-23 18:27:58 -0600

    Updating top header

--- js/application.js
+++ js/application.js
@@ -2,6 +2,7 @@ var resourceLoader;
 
 App.onLaunch = function(options) {
 	var javascriptFiles = [
+	 `${options.BASEURL}js/mixpanel.js`,
 	 `${options.BASEURL}js/ResourceLoader.js`,
 	 `${options.BASEURL}js/Presenter.js`
 	];
@@ -9,11 +10,12 @@ App.onLaunch = function(options) {
 	evaluateScripts(javascriptFiles, function(success) {
 		if(success) {
 			resourceLoader = new ResourceLoader(options.BASEURL);
-			resourceLoader.loadResource(`${options.BASEURL}templates/pinoytv.xml.js`, function(resource) {
-				var doc = Presenter.makeDocument(resource);
-				doc.addEventListener("select", Presenter.load.bind(Presenter));
-				Presenter.pushDocument(doc);
-			})
+			resourceLoader.loadResource(`${options.BASEURL}templates/pinoytv.xml.js`, 
+				function(resource) {
+					var doc = Presenter.makeDocument(resource);
+					doc.addEventListener("select", Presenter.load.bind(Presenter));
+					Presenter.pushDocument(doc);
+			});
 		} else {
 			var errorDoc = createAlert("Evaluate Scripts Error", "Error attempting to evaluate external JavaScript files.");
 			navigationDocument.presentModal(errorDoc);
